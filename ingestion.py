72e9145332d00d47fda0a0f258bfe0ba05d6b10f
laynejohnson13 <laynejohnson13001@gmail.com>
2022-08-28 00:24:51 -0400

    File path renamed

--- ingestion.py
+++ ingestion.py
@@ -3,13 +3,19 @@ import pandas as pd
 import xlrd ## import xlrd for excel files, tab names 
 
 ##import local xls file 
-df = pd.read_excel('covid19tracker.xls')
+df = pd.read_excel('/Users/laynejohnson/Documents/GitHub/hha-data-ingestion/Data/covid19tracker.xls')
 
 
-xls = xlrd.open_workbook('covid19tracker.xls', on_demand=True)
+xls = xlrd.open_workbook('/Users/laynejohnson/Documents/GitHub/hha-data-ingestion/Data/covid19tracker.xls', on_demand=True)
 xls.sheet_names()
 
 ###xls to dataframe
-df = pd.read_excel('covid19tracker.xls', sheet_name= 'Health Policy Response')
-df = pd.read_excel('covid19tracker.xls', sheet_name= 'Country coverage')
-df = pd.read_excel('covid19tracker.xls', sheet_name= 'Other Policy Tracking Sites')
+df = pd.read_excel('/Users/laynejohnson/Documents/GitHub/hha-data-ingestion/Data/covid19tracker.xls', sheet_name= 'Health Policy Response')
+df = pd.read_excel('/Users/laynejohnson/Documents/GitHub/hha-data-ingestion/Data/covid19tracker.xls', sheet_name= 'Country coverage')
+df = pd.read_excel('/Users/laynejohnson/Documents/GitHub/hha-data-ingestion/Data/covid19tracker.xls', sheet_name= 'Other Policy Tracking Sites')
+
+
+###tabs named
+tab1 = pd.read_excel('/Users/laynejohnson/Documents/GitHub/hha-data-ingestion/Data/covid19tracker.xls', sheet_name= 'Health Policy Response')
+tab2 = pd.read_excel('/Users/laynejohnson/Documents/GitHub/hha-data-ingestion/Data/covid19tracker.xls', sheet_name= 'Country coverage')
+tab3 = pd.read_excel('/Users/laynejohnson/Documents/GitHub/hha-data-ingestion/Data/covid19tracker.xls', sheet_name= 'Other Policy Tracking Sites')
