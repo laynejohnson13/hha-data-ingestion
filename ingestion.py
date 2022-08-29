250277a6d557aa559040078ba723a3d6562115c6
laynejohnson13 <laynejohnson13001@gmail.com>
2022-08-29 11:51:29 -0400

    Section 3 started

--- ingestion.py
+++ ingestion.py
@@ -3,6 +3,7 @@ import pandas as pd
 import xlrd 
 import requests
 import json
+from google.cloud import bigquery
 
 #####SECTION1
 
@@ -30,3 +31,17 @@ apiDataset = apiDataset.json()
 
 
 ###SECTION3
+
+##Creating client variable to connect to authentication key
+client = bigquery.Client.from_service_account_json('/Users/laynejohnson/Desktop/data-ingestion-360914-3248b6fda89f.json')
+
+##Creating query_job to access public database
+query_job = client.query("SELECT * FROM `bigquery-public-data.covid19_italy` LIMIT 100")
+
+##Results
+results = query_job.result() 
+###receiving error- bigquery-public-data was not found in location US
+
+##Results into dataframe
+bigquery1 = pd.DataFrame(results.to_dataframe()) 
+###recieving error - 'results' is  not defined
